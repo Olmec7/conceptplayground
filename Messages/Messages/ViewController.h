01a482d113b7ff66476cd791aec12960acdbf14b
Olmec7 <chadslater@Chads-MacBook-Pro-2.local>
2014-10-27 10:14:21 -0600

    messages

---
+++ Messages/Messages/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Messages
+//
+//  Created by Chad Slater on 10/27/14.
+//  Copyright (c) 2014 Chad Slater. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Messages/Messages/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Messages
+//
+//  Created by Chad Slater on 10/27/14.
+//  Copyright (c) 2014 Chad Slater. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
